669abe2dcac63822d14f83922518740ccc65e503
STEVE KORNAHRENS <steve.kornahrens@gmail.com>
2017-09-21 18:41:59 -0600

    db final

--- seed_data/trip_seed.js
+++ seed_data/trip_seed.js
@@ -4,9 +4,4 @@ module.exports = [
     start_date: '1505517889000',
     end_date: '1506122689000'
   },
-  {
-    name: 'Beach 2017',
-    start_date: '1514160000',
-    end_date: '1514678400'
-  },
 ]
