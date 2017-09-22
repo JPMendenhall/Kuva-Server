0c85081366d7fbe840354abd55d32d348e500bdb
STEVE KORNAHRENS <steve.kornahrens@gmail.com>
2017-09-22 13:17:07 -0600

    cors update

--- app.js
+++ app.js
@@ -20,11 +20,12 @@ const login = require('./api/login_api')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors())
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors)
+
 
 app.use('/api/patrons', patrons)
 app.use('/api/trips', trips)
